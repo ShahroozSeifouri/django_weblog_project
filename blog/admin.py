358c52605c1fb2f230e8327f6ff7dad93cec687b
unknown <ssayfouri@gmail.com>
2022-07-31 21:50:46 +0430

    first

---
+++ blog/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title' , 'status', 'author' ,'datetime_created','datetime_modified')
+    ordering = ('-datetime_created' ,)
+
+# admin.site.register(Post ,PostAdmin)
